f65428e45eab19bf84f092ee2c841ffd6c01d4e0
Randy Uebel <ruebel@gsandf.com>
2017-06-27 16:14:55 -0500

    change bg

--- src/modules/Sequencer/components/NoteGrid/Row/Cell/index.js
+++ src/modules/Sequencer/components/NoteGrid/Row/Cell/index.js
@@ -1,21 +1,29 @@
 import React, { PropTypes } from 'react';
 
-const colors = ['#75ABDC', '#FAD394', '#35B780', '#F2F2F2'];
+const colors = ['117, 171, 220', '250, 211, 148', '53, 183, 128'];
+const getColor = (beat, hasNote, on) => {
+  if (on) {
+    return 'rebeccapurple';
+  } else {
+    return `rgba(${colors[beat - 1]}, ${hasNote ? '1' : '0.3'})`;
+  }
+};
 
-const getStyle = (beat, on) => ({
-  backgroundColor: on ? 'rebeccapurple' : colors[beat - 1],
+const getStyle = (beat, hasNote, on) => ({
+  backgroundColor: getColor(beat, hasNote, on),
   color: on ? 'white' : 'black',
   minWidth: '20px',
   width: '45px'
 });
 
 const Cell = ({ addNote, beat, id, note, on, removeNote, tone }) => {
+  const hasNote = note && note.velocity > 0;
   return (
     <td
       onClick={() => (note ? removeNote(note) : addNote({ beat: id, tone }))}
-      style={getStyle(beat, on)}
+      style={getStyle(beat, hasNote, on)}
     >
-      {note && note.velocity > 0 && <span>{note.tone}</span>}
+      {hasNote && <span>{note.tone}</span>}
     </td>
   );
 };
